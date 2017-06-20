eae6e120a69dc775fd1245dc8a7c2ef2470fa8dc
Keith Mendoza <keith@homepluspower.info>
2017-06-20 09:04:16 -0700

    Fixed file type defines + extra comments

--- playdohfs.h
+++ playdohfs.h
@@ -29,13 +29,16 @@ const size_t playdohfs_sb_size = sizeof(struct playdohfs_super_block);
 /*
     Structure for 1 inode
 */
-#define PLAYDOHFS_INODE_FREE    0x0
-#define PLAYDOHFS_INODE_DIR     0x1
-#define PLAYDOHFS_INODE_FILE    0x2
+#define PLAYDOHFS_INODE_FREE    0x0000
+#define PLAYDOHFS_INODE_DIR     0x0100
+#define PLAYDOHFS_INODE_FILE    0x0200
 struct __attribute__ ((__packed__)) playdohfs_inode
 {
     char filename[PLAYDOHFS_MAX_FILENAME+1];
     uint32_t filesize;
+
+    /* For now only put files in 1 block, which will limit the size we can store
+        to 4096 */
     uint32_t blockpos;
 
      /* Mode and inode type. 
